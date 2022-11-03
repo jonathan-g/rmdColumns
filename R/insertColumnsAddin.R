23c864f88a345fe2294559a62ced74967ae1028f
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2022-11-02 22:58:58 -0500

    Updated documentation and functions. It now works.

--- R/insertColumnsAddin.R
+++ R/insertColumnsAddin.R
@@ -1,3 +1,10 @@
+#' Insert Rmarkdown divs for a two-column layout
+#'
+#' Insert Rmarkdown divs for a two-column layout into the current
+#' document. Uses the pandoc `:::` ... `:::` syntax for divs,
+#' with `{.columns}` and `{.column}` tags for column sets and individual
+#' columns.
+#'
 insertColumnsAddin <- function() {
   doc <- rstudioapi::getSourceEditorContext()
   row <- rstudioapi::primary_selection(doc)$range$start['row']
@@ -13,8 +20,10 @@ insertColumnsAddin <- function() {
     text = paste(
       "::::::::: {.columns}",
       ":::::: {.column}",
+      "",
       "::::::",
       ":::::: {.column}",
+      "",
       "::::::",
       ":::::::::",
       "",
@@ -22,4 +31,34 @@ insertColumnsAddin <- function() {
     ),
     id = doc$id
   )
+  rstudioapi::setCursorPosition(c(row + 2, 1), id = doc$id)
+}
+
+#' Insert Rmarkdown div for an extra column
+#'
+#' Insert Rmarkdown div for a single column into a multi-column layout.
+#' Uses the pandoc `:::` ... `:::` syntax for divs, with a `{.column}` tag.
+#'
+insertOneColumnAddin <- function() {
+  doc <- rstudioapi::getSourceEditorContext()
+  row <- rstudioapi::primary_selection(doc)$range$start['row']
+  col <- rstudioapi::primary_selection(doc)$range$start['column']
+  rstudioapi::setCursorPosition(c(row, Inf), id = doc$id)
+  new_pos <- rstudioapi::primary_selection(doc)
+  if (new_pos$range$start['column'] > 1) {
+    rstudioapi::insertText(new_pos$start, "\n", doc$id)
+    row <- row + 1
+  }
+  rstudioapi::insertText(
+    location = c(row, 1),
+    text = paste(
+      ":::::: {.column}",
+      "",
+      "::::::",
+      "",
+      sep = "\n"
+    ),
+    id = doc$id
+  )
+  rstudioapi::setCursorPosition(c(row + 1, 1), id = doc$id)
 }

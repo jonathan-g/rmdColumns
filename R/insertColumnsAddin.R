0da9681e5da61c8ca61cde5e76f3d544a3b54330
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2022-11-03 03:15:09 -0500

    Added a few more commands.

--- R/insertColumnsAddin.R
+++ R/insertColumnsAddin.R
@@ -62,3 +62,87 @@ insertOneColumnAddin <- function() {
   )
   rstudioapi::setCursorPosition(c(row + 1, 1), id = doc$id)
 }
+
+#' Insert Rmarkdown column break
+#'
+#' Insert Rmarkdown code to end a column div and start another
+#'
+insertColumnBreakAddin <- function() {
+  doc <- rstudioapi::getSourceEditorContext()
+  row <- rstudioapi::primary_selection(doc)$range$start['row']
+  col <- rstudioapi::primary_selection(doc)$range$start['column']
+  rstudioapi::setCursorPosition(c(row, Inf), id = doc$id)
+  new_pos <- rstudioapi::primary_selection(doc)
+  if (new_pos$range$start['column'] > 1) {
+    rstudioapi::insertText(new_pos$start, "\n", doc$id)
+    row <- row + 1
+  }
+  rstudioapi::insertText(
+    location = c(row, 1),
+    text = paste(
+      "::::::",
+      ":::::: {.column}",
+      "",
+      sep = "\n"
+    ),
+    id = doc$id
+  )
+  rstudioapi::setCursorPosition(c(row + 2, 1), id = doc$id)
+}
+
+#' Insert Rmarkdown max-listing div
+#'
+#' Insert div with `.maxlisting` class.
+#'
+insertMaxListingAddin <- function() {
+  doc <- rstudioapi::getSourceEditorContext()
+  row <- rstudioapi::primary_selection(doc)$range$start['row']
+  col <- rstudioapi::primary_selection(doc)$range$start['column']
+  rstudioapi::setCursorPosition(c(row, Inf), id = doc$id)
+  new_pos <- rstudioapi::primary_selection(doc)
+  if (new_pos$range$start['column'] > 1) {
+    rstudioapi::insertText(new_pos$start, "\n", doc$id)
+    row <- row + 1
+  }
+  rstudioapi::insertText(
+    location = c(row, 1),
+    text = paste(
+      "::: {.max-listing}",
+      ":::",
+      "",
+      sep = "\n"
+    ),
+    id = doc$id
+  )
+  rstudioapi::setCursorPosition(c(row + 1, 1), id = doc$id)
+}
+
+#' Insert LaTeX displaymath align block
+#'
+#' Insert LaTeX displaymath blosk with an align environment.
+#'
+insertDisplayAlignAddin <- function() {
+  doc <- rstudioapi::getSourceEditorContext()
+  row <- rstudioapi::primary_selection(doc)$range$start['row']
+  col <- rstudioapi::primary_selection(doc)$range$start['column']
+  rstudioapi::setCursorPosition(c(row, Inf), id = doc$id)
+  new_pos <- rstudioapi::primary_selection(doc)
+  if (new_pos$range$start['column'] > 1) {
+    rstudioapi::insertText(new_pos$start, "\n", doc$id)
+    row <- row + 1
+  }
+  rstudioapi::insertText(
+    location = c(row, 1),
+    text = paste(
+      "$$",
+      "\\begin{align}",
+      "",
+      "\\end{align}",
+      "$$",
+      "",
+      sep = "\n"
+    ),
+    id = doc$id
+  )
+  rstudioapi::setCursorPosition(c(row + 2, 1), id = doc$id)
+}
